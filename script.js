fb1eb38f4ed3c912ad4e26d59ff0f53e7d223fa0
Hay Abadi <abados9891@gmail.com>
2023-07-04 15:33:55 +0300

    finishStep3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const API_KEY = "28925108-9358d987526d525ae413b44c1";
 let currentPage = 1;
-let images = []; // Declare an empty array to store the fetched images
+let images = [];
+let favoriteImages = [];
 
 // Add an event listener to the search button
 document.getElementById("searchButton").addEventListener("click", function () {
@@ -10,168 +11,218 @@ document.getElementById("searchButton").addEventListener("click", function () {
 });
 
 // Add an event listener to the "More Images" button
-document.getElementById("moreImagesButton").addEventListener("click", function () {
-  var searchQuery = document.getElementById("searchInput").value;
-  currentPage++;
-  fetchImages(searchQuery, currentPage);
-});
+document
+  .getElementById("moreImagesButton")
+  .addEventListener("click", function () {
+    var searchQuery = document.getElementById("searchInput").value;
+    currentPage++;
+    fetchImages(searchQuery, currentPage);
+  });
 
-// Add an event listener to the image container for opening the modal
-document.getElementById("imageContainer").addEventListener("click", function (event) {
-  if (event.target.classList.contains("image-card")) {
-    var imageIndex = Array.from(this.children).indexOf(event.target);
-    var image = images[imageIndex];
-    showModal(image);
-  }
-});
+// Add event delegation for the favorite icon click event
+document
+  .getElementById("imageContainer")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("favorite-icon")) {
+      var imageIndex = Array.from(
+        event.target.parentNode.parentNode.children
+      ).indexOf(event.target.parentNode);
+      var image = images[imageIndex];
+      image.favorite = !image.favorite;
 
-var currentSearchQuery = "";
-
-function fetchImages(query, param) {
-    var imageContainer = document.getElementById("imageContainer");
-  
-    if (typeof param === "string") {
-      var tagFilter = param;
-      // Remove existing images from the container
-      imageContainer.innerHTML = "";
-  
-      // Construct the API URL with the search query and tag filter
-      var apiUrl = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + query;
-  
-      if (tagFilter) {
-        apiUrl += "&category=" + tagFilter;
+      if (image.favorite) {
+        event.target.innerHTML = "&#9733;"; // Change the icon to a filled star
+        event.target.style.backgroundColor = "green"; // Highlight the star
+        favoriteImages.push(image); // Add the image to the favoriteImages array
+      } else {
+        event.target.innerHTML = "&#9734;"; // Change the icon to an empty star
+        event.target.style.backgroundColor = "transparent"; // Remove the highlight from the star
+        favoriteImages = favoriteImages.filter(function (favImage) {
+          return favImage !== image; // Remove the image from the favoriteImages array
+        });
       }
-  
-      fetch(apiUrl)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          images = data.hits; // Store the fetched images in the 'images' array
-          displayImages(images, query);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    } else if (typeof param === "number") {
-      var page = param;
-  
-      fetch(
-        "https://pixabay.com/api/?key=" +
-          API_KEY +
-          "&q=" +
-          query +
-          "&page=" +
-          page
-      )
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          var newImages = data.hits; // Get the new fetched images
-          images = images.concat(newImages); // Append the new images to the 'images' array
-          displayImages(newImages, query); // Display only the new images
-          currentPage = page;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
     }
+  });
+
+// Function to fetch images from the API
+function fetchImages(query, param) {
+  var imageContainer = document.getElementById("imageContainer");
+
+  if (typeof param === "string") {
+    var tagFilter = param;
+    // Remove existing images from the container
+    imageContainer.innerHTML = "";
+
+    // Construct the API URL with the search query and tag filter
+    var apiUrl = "https://pixabay.com/api/?key=" + API_KEY + "&q=" + query;
+
+    if (tagFilter) {
+      apiUrl += "&category=" + tagFilter;
+    }
+
+    fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        images = data.hits; // Store the fetched images in the 'images' array
+        displayImages(images, query); // Display the images in the image container
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } else if (typeof param === "number") {
+    var page = param;
+
+    fetch(
+      "https://pixabay.com/api/?key=" +
+        API_KEY +
+        "&q=" +
+        query +
+        "&page=" +
+        page
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var newImages = data.hits; // Get the newly fetched images
+        images = images.concat(newImages); // Append the new images to the 'images' array
+        displayImages(newImages, query); // Display only the new images
+        currentPage = page;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
-  
+}
 
-var favoriteImages = [];
-
+// Function to display images in the image container
 function displayImages(images, searchQuery) {
-    var imageContainer = document.getElementById("imageContainer");
-    var moreImagesButton = document.getElementById("moreImagesButton");
-  
-    
-  
-    for (var i = 0; i < images.length; i++) {
-      var image = images[i];
-      image.favorite = false;
-  
-      var imageCard = document.createElement("div");
-      imageCard.classList.add("image-card");
-  
-      var imageElement = document.createElement("img");
-      imageElement.src = image.webformatURL;
-      imageElement.alt = image.tags;
-  
-      var favoriteIcon = document.createElement("span");
-      favoriteIcon.classList.add("favorite-icon");
-      favoriteIcon.innerHTML = "&#9734;";
-  
-      favoriteIcon.addEventListener("click", function (event) {
-        event.stopPropagation();
-        image.favorite = !image.favorite;
-  
-        if (image.favorite) {
-          this.innerHTML = "&#9733;";
-          this.style.backgroundColor = "green";
-        } else {
-          this.innerHTML = "&#9734;";
-          this.style.backgroundColor = "transparent";
-        }
-  
-        console.log(images);
-      });
-  
-      imageCard.appendChild(favoriteIcon);
-      imageCard.appendChild(imageElement);
-      imageContainer.appendChild(imageCard);
-  
-      // Add event listener to imageCard for opening the modal
-      imageCard.addEventListener("click", function (event) {
-        var imageIndex = Array.from(this.parentNode.children).indexOf(this);
-        var image = images[imageIndex];
-        showModal(image);
-      });
+  var imageContainer = document.getElementById("imageContainer");
+  var moreImagesButton = document.getElementById("moreImagesButton");
+
+  for (var i = 0; i < images.length; i++) {
+    var image = images[i];
+    image.favorite = false; // Set the favorite property to false initially
+
+    var imageCard = document.createElement("div");
+    imageCard.classList.add("image-card");
+
+    var imageElement = document.createElement("img");
+    imageElement.src = image.webformatURL;
+    imageElement.alt = image.tags;
+    imageElement.title = image.tags; // Set the title attribute to display image.tags on hover
+
+    var favoriteIcon = document.createElement("span");
+    favoriteIcon.classList.add("favorite-icon");
+    favoriteIcon.innerHTML = "&#9734;"; // Set the icon as an empty star
+
+    favoriteIcon.addEventListener("click", function (event) {
+      event.stopPropagation();
+      var imageIndex = Array.from(this.parentNode.parentNode.children).indexOf(
+        this.parentNode
+      );
+      var image = images[imageIndex];
+      image.favorite = !image.favorite;
+
+      if (image.favorite) {
+        this.innerHTML = "&#9733;"; // Change the icon to a filled star
+        this.style.backgroundColor = "green"; // Highlight the star
+        favoriteImages.push(image); // Add the image to the favoriteImages array
+      } else {
+        this.innerHTML = "&#9734;"; // Change the icon to an empty star
+        this.style.backgroundColor = "transparent"; // Remove the highlight from the star
+        favoriteImages = favoriteImages.filter(function (favImage) {
+          return favImage !== image; // Remove the image from the favoriteImages array
+        });
+      }
+
+      console.log(favoriteImages);
+    });
+
+    // Check if the image is in the favoriteImages array and mark it as a star
+    if (
+      favoriteImages.some(function (favImage) {
+        return favImage.id === image.id;
+      })
+    ) {
+      favoriteIcon.innerHTML = "&#9733;"; // Change the icon to a filled star
+      favoriteIcon.style.backgroundColor = "green"; // Highlight the star
+      image.favorite = true;
     }
-  
-    moreImagesButton.style.display = images.length > 0 ? "block" : "none";
+
+    imageCard.appendChild(favoriteIcon);
+    imageCard.appendChild(imageElement);
+    imageContainer.appendChild(imageCard);
+
+    // Add event listener to imageCard for opening the modal
+    imageCard.addEventListener("click", function (event) {
+      var imageIndex = Array.from(this.parentNode.children).indexOf(this);
+      var image = images[imageIndex];
+      showModal(image);
+    });
   }
-  
+
+  moreImagesButton.style.display = images.length > 0 ? "block" : "none"; // Show or hide the "More Images" button
+}
 
 function showModal(image) {
-var modal = document.getElementById("modal");
-var modalContent = document.querySelector("#modal .modal-content");
-modalContent.innerHTML = "";
+  // Get the modal and modal content elements
+  var modal = document.getElementById("modal");
+  var modalContent = document.querySelector("#modal .modal-content");
+  modalContent.innerHTML = "";
 
-var imageElement = document.createElement("img");
-imageElement.src = image.largeImageURL;
-imageElement.alt = image.tags;
+  // Create the image element and set its source and alt attributes
+  var imageElement = document.createElement("img");
+  imageElement.src = image.largeImageURL;
+  imageElement.alt = image.tags;
 
-var isFavorite = image.favorite;
+  // Check if the image is marked as a favorite
+  var isFavorite = image.favorite;
 
-var favoriteIcon = document.createElement("span");
-favoriteIcon.classList.add("favorite-icon");
-favoriteIcon.innerHTML = isFavorite ? "&#9733;" : "&#9734;";
+  // Create the favorite icon element
+  var favoriteIcon = document.createElement("span");
+  favoriteIcon.classList.add("favorite-icon");
+  favoriteIcon.innerHTML = isFavorite ? "&#9733;" : "&#9734;";
 
-favoriteIcon.addEventListener("click", function (event) {
-  event.stopPropagation();
-  image.favorite = !image.favorite;
+  // Add a click event listener to the favorite icon
+  favoriteIcon.addEventListener("click", function (event) {
+    event.stopPropagation();
+    // Get the index of the image in the 'images' array
+    var imageIndex = Array.from(this.parentNode.parentNode.children).indexOf(
+      this.parentNode
+    );
+    var image = images[imageIndex];
+    // Toggle the favorite status of the image
+    image.favorite = !image.favorite;
 
-  if (image.favorite) {
-    this.innerHTML = "&#9733;";
-    this.style.backgroundColor = "green";
-  } else {
-    this.innerHTML = "&#9734;";
-    this.style.backgroundColor = "transparent";
-  }
+    if (image.favorite) {
+      this.innerHTML = "&#9733;";
+      this.style.backgroundColor = "green";
+      favoriteImages.push(image); // Add the image to the 'favoriteImages' array
+    } else {
+      this.innerHTML = "&#9734;";
+      this.style.backgroundColor = "transparent";
+      favoriteImages = favoriteImages.filter(function (favImage) {
+        return favImage !== image;
+      }); // Remove the image from the 'favoriteImages' array
+    }
 
-  console.log(images);
-});
+    console.log(favoriteImages);
+  });
 
-modalContent.appendChild(favoriteIcon);
-modalContent.appendChild(imageElement);
+  // Append the favorite icon and image element to the modal content
+  modalContent.appendChild(favoriteIcon);
+  modalContent.appendChild(imageElement);
 
-modal.style.display = "block";
+  // Display the modal
+  modal.style.display = "block";
 
-modal.addEventListener("click", function (e) {
-  if (e.target === modal || e.target.classList.contains("close")) {
-    modal.style.display = "none";
-  }
-});
+  // Add a click event listener to close the modal when clicking outside or on the close button
+  modal.addEventListener("click", function (e) {
+    if (e.target === modal || e.target.classList.contains("close")) {
+      modal.style.display = "none";
+    }
+  });
 }
