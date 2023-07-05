699dd42da394099f10ead7becb706f3d2cd2a0bd
Hay Abadi <abados9891@gmail.com>
2023-07-05 17:57:49 +0300

    DoneStep4IncludeCaching

--- backend/server.js
+++ backend/server.js
@@ -5,39 +5,63 @@ const app = express();
 const API_KEY = "28925108-9358d987526d525ae413b44c1";
 const PORT = 3000;
 
+// Cache object to store the query and corresponding results
+const cache = {};
+const cacheKeys = [];
+
 // Serve the static files from the client directory
 app.use(express.static("../client"));
 
 // Route for handling the search API call
 app.get("/api/search", (req, res) => {
-    const { query, tagFilter } = req.query;
+  const { query, tagFilter } = req.query;
+  const cacheKey = `${query}`;
+
+  // Check if the query is present in the cache
+  if (cache[cacheKey]) {
+    // If the query is cached, return the cached result
+    res.json(cache[cacheKey]);
+    console.log(cache[cacheKey]);
+  } else {
+    // If the query is not cached, make an API call
     const apiUrl = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&category=${tagFilter}`;
-  
+
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
         res.json(data);
+
+        // Add the query and result to the cache
+        cache[cacheKey] = data;
+        cacheKeys.push(cacheKey);
+
+        // Check if the cache size exceeds 5
+        if (cacheKeys.length > 5) {
+          const oldestCacheKey = cacheKeys.shift();
+          delete cache[oldestCacheKey];
+        }
       })
       .catch((error) => {
         console.log(error);
         res.status(500).json({ error: "An error occurred" });
       });
-  });
+  }
+});
 
 // Route for handling the random images API call
 app.get("/api/random", (req, res) => {
-    const apiUrl = `https://pixabay.com/api/?key=${API_KEY}&per_page=10`;
-  
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json({ error: "An error occurred" });
-      });
-  });
+  const apiUrl = `https://pixabay.com/api/?key=${API_KEY}&per_page=10`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "An error occurred" });
+    });
+});
 
 // Route for handling the more images API call
 app.get("/api/moreImages", (req, res) => {
